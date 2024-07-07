e68fda50e1cdf2d01beaf6290c13e94547251d4a
achbintangfebrian <bintangfebrian1002@gmail.com>
2024-07-07 15:33:26 +0700

    modif warna dan selesai project nya

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -10,10 +10,10 @@ const Detail = ({route, navigation}) => {
           <StatusBar backgroundColor="#0288d1" barStyle="light-content" />
           
           <View style={{padding: 20, backgroundColor: '#03a9f4', elevation: 1, flexDirection: 'row', alignItems: 'center' }}>
-            <Icon name="arrow-left" size={25} color="white"  style={{marginRight: 10}} onPress={() => navigation.goBack()}/>
+            <Icon name="arrow-left" size={25} color="white"  style={{marginRight: 13}} onPress={() => navigation.goBack()}/>
             <Text style={{textAlign: 'center', color: '#FFFFFF', fontWeight: 'bold', fontSize: 20}}>Detail</Text>
           </View>
-          <View style={{backgroundColor: 'grey', paddingBottom: 10, margin: 5, borderRadius: 10}}>
+          <View style={{backgroundColor: '#BDBDBD', paddingBottom: 10, margin: 5, borderRadius: 10}}>
           <Text style={{textAlign: 'center', marginTop: 20, fontWeight: 'bold', fontSize: 20, color: 'white'}}>{indonesia}</Text>
           <Text style={{textAlign: 'center', marginTop: 5, fontSize: 17, color:'white'}}>{english}</Text>
           </View>
